79eade896d2a2d50d1a6a92810e08829adca01a0
TiavinaMichael <ralainirinatiavina@gmail.com>
2019-12-07 17:39:15 +0100

    update animation

--- src/index.js
+++ src/index.js
@@ -1,117 +1,146 @@
 import ReactDOM from "react-dom";
-import React from 'react';
-import { makeStyles, darken, lighten } from '@material-ui/core/styles';
+import React from "react";
+import clsx from "clsx";
+import { makeStyles, darken } from "@material-ui/core/styles";
 import { motion } from "framer-motion";
-
+import { useCountUp } from 'react-countup';
 
 const useStyles = makeStyles({
   circleIn: {
-    fill: 'url(#gradientIn)', 
-    stroke: 'red',
-    strokeWidth: 10,
+    fill: "url(#gradientIn)",
+    strokeWidth: 10
+  },
+  circleInStart: {
+    stroke: "red",
+  },
+  circleInEnd: {
+    stroke: "green",
   },
   circleOut: {
-    fill: 'url(#gradientOut)', 
-    stroke: 'black'
+    // fill: 'url(#gradientOut)',
+    stroke: '#000',
+    strokeMiterlimit: '10',
+    strokeWidth: '2px',
   },
+  circleOutActive: {
+    fill: 'url(#gradientOutActive)',
+    animation: '$setCircleOutBg 2s linear infinite forwards'
+  },
+  circleOutCompleted: {
+    fill: 'url(#gradientOutCompleted)',
+  },
+  '@keyframes setCircleOutBg': {
+      '100%': {
+        fill: 'url(#gradientOutActive)'
+      }
+  },
+  '@keyframes setBorderCircle': {
+    '100%': {
+      stroke: darken('#fff', 0.6)
+    }
+},
   circleInBorder: {
-    fill: 'none', 
-    stroke: 'black',
+    fill: "none",
+    stroke: "black",
+    animation: '$setBorderCircle 2s linear infinite forwards',
     strokeWidth: 2
   },
   line: {
-      fill: 'pink', 
-      stroke: 'pink', 
-      strokeWidth: '10',
-      strokeLinecap: 'round'
+    fill: "pink",
+    stroke: "pink",
+    strokeWidth: "10",
+    strokeLinecap: "round"
   },
   stop1: {
-    stopColor: 'red',
+    stopColor: "red",
     stopOpacity: 1
   },
   stop2: {
-    stopColor: 'green',
+    stopColor: "green",
     stopOpacity: 1
   },
-
+  count: {
+    fill: '#fff',
+    fontSize: 35
+  }
 });
 
-const App = ({ variants, onCompleted}) => {
-    const classes = useStyles();
+const App = () => {
+  const classes = useStyles();
+  const { countUp } = useCountUp({ end: 100 });
 
-    return (
-        <svg width="300" height="300" viewBox="0, 0, 300, 300">
-            <defs>
-              <linearGradient id="grad1" x1="0%" y1="0%" x2="100%" y2="0%">
-                <stop offset="0%"  className={classes.stop1} />
-                <stop offset="100%"  className={classes.stop2} />
-              </linearGradient>
+  return (
+    <svg width="300" height="300" viewBox="0, 0, 300, 300">
+      <defs>
+        <linearGradient id="grad1" x1="0%" y1="0%" x2="100%" y2="0%">
+          <stop offset="0%" className={classes.stop1} />
+          <stop offset="100%" className={classes.stop2} />
+        </linearGradient>
 
-              <radialGradient id = "gradientOut" cx = "50%" cy = "50%" r = "55%">
-                  <stop stopColor = "transparent" offset = "0%"/>
-                  <stop stopColor = "#757575" offset = "40%"/>
-                  <stop stopColor = {darken("#757575", 0.8)} offset = "90%"/>
-                  <stop stopColor =  "#fff" offset = "100%"/>
-              </radialGradient>
+        <radialGradient id="gradientOutActive" cx="50%" cy="50%" r="55%">
+          <stop stopColor="transparent" offset="0%" />
+          <stop stopColor="red" offset="40%" />
+          <stop stopColor="#000" offset="90%" />
+          <stop stopColor="#000" offset="100%" />
+        </radialGradient>
+        <radialGradient id="gradientOutCompleted" cx="50%" cy="50%" r="55%">
+          <stop stopColor="transparent" offset="0%" />
+          <stop stopColor="green" offset="40%" />
+          <stop stopColor="#000" offset="90%" />
+          <stop stopColor="#000" offset="100%" />
+        </radialGradient>
+        <radialGradient id="gradientOutActive" cx="50%" cy="50%" r="55%">
+          <stop stopColor="transparent" offset="0%" />
+          <stop stopColor="#fff" offset="40%" />
+          <stop stopColor="#000" offset="90%" />
+          <stop stopColor="#000" offset="100%" />
+        </radialGradient>
 
-              <radialGradient id = "gradientIn" cx = "50%" cy = "50%" r = "55%">
-                  <stop stopColor = "#000" offset = "0%"/>
-                  <stop stopColor = "#000" offset = "40%"/>
-                  <stop stopColor = "#000" offset = "90%"/>
-                  <stop stopColor = '#000' offset = "100%"/>
-              </radialGradient>
+        <radialGradient id="gradientIn" cx="50%" cy="50%" r="55%">
+          <stop stopColor="#000" offset="0%" />
+          <stop stopColor="#000" offset="40%" />
+          <stop stopColor="#000" offset="90%" />
+          <stop stopColor="#000" offset="100%" />
+        </radialGradient>
+      </defs>
+      <g>
+        <circle
+          cx={150}
+          cy={150}
+          r={130}
+          // id="orange-circle"
+          className={clsx(
+            classes.circleOut,
+            countUp < 100 ? classes.circleOutActive: classes.circleOutCompleted
+          )}
+          // fill= 'url(#gradientOut)'
+          // animate={{ cy: [null, 200], r: [115, 125] }}
+          // transition={{
+          //   duration: 1,
+          //   times: [0, 1],
+          //   yoyo: Infinity,
+          // }}
+        />
 
-          </defs>
-          <g>
-          <circle
-                cx={150}
-                cy={150}
-                r={130}
-                // id="orange-circle"
-                className={classes.circleOut}
-                // fill= 'url(#gradientOut)'
-                // animate={{ cy: [null, 200], r: [115, 125] }}
-                // transition={{ 
-                //   duration: 1,
-                //   times: [0, 1], 
-                //   yoyo: Infinity, 
-                // }}
-            >
-            <animateColor 
-                // xlinkHref="#orange-circle"
-                attributeName="fill"
-                attributeType="CSS"
-                from='url(#gradientOut)'
-                to='url(#grad1)' 
-                dur="3s"
-                repeatCount="indefinite"
-                // begin="click"
-                fill="freeze"
-            />
-          </circle>
-          <circle
-              cx={150}
-              cy={150}
-              r={106}
-              className={classes.circleInBorder}
-          />
-           <motion.circle
-              cx={150}
-              cy={150}
-              r={100}
-              className={classes.circleIn}
-          />
-           <circle
-              cx={150}
-              cy={150}
-              r={95}
-              className={classes.circleInBorder}
-          />
-
-          </g>
-        </svg> 
-    )
-}
+        <circle cx={150} cy={150} r={106} className={classes.circleInBorder} />
+        <motion.circle cx={150} cy={150} r={100}
+          className={clsx(
+            classes.circleIn, 
+            countUp < 100 ? classes.circleInStart: classes.circleInEnd
+          )} />
+        <text 
+          x={150} y={150}
+          className={classes.count}
+          strokeWidth="2px"
+          textAnchor="middle"
+          alignmentBaseline="middle">
+            {countUp}%
+        </text>
+        <circle cx={150} cy={150} r={95} className={classes.circleInBorder} />
+      </g>
+    </svg>
+  );
+};
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
